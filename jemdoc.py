7b68f4a1c59225ca89977797fb6c4b5ab2769d45
joycezhangjia <joycezhangjia@gmail.com>
2026-09-08 02:23:10 +0000

    Clarify publication hierarchy

--- jemdoc.py
+++ jemdoc.py
@@ -140,7 +140,7 @@ def standardconf():
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   
   [defaultcss]
-  <link rel="stylesheet" href="jemdoc.css?v=20260908" type="text/css" />
+  <link rel="stylesheet" href="jemdoc.css?v=20260908-pubs" type="text/css" />
   
   [windowtitle]
   # used in header for window title.
